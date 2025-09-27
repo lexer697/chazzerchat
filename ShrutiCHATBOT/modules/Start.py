ad474fb921afb6fc1d8726679ac4d91433ecf512
lexer697 <music1ygffygfgdf@gmail.com>
2025-09-27 20:40:57 +0530

    Update Start.py

--- ShrutiCHATBOT/modules/Start.py
+++ ShrutiCHATBOT/modules/Start.py
@@ -49,7 +49,7 @@ EMOJIOS = [
     "🕊",
 ]
 
-BOT = "https://envs.sh/IL_.jpg"
+BOT = "https://files.catbox.moe/9px2me.jpg"
 IMG = [
     "https://graph.org/file/210751796ff48991b86a3.jpg",
     "https://graph.org/file/7b4924be4179f70abcf33.jpg",
